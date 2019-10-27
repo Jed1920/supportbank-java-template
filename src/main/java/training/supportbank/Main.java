399d9df1d2624f7ad59ff2c345728798959f2b4e
Jed Johnson <jed.johnson@softwire.com>
2019-10-27 14:20:32 +0000

    Trying to Setup

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -3,6 +3,9 @@ package training.supportbank;
 public class Main {
     public static void main(String args[]) {
         // Your code here!
-        System.out.println("Test!");
+        String b = "Test!";
+        System.out.println(b);
+
+
     }
 }
